da64180aaea7d449b6c12f14decfa7d3dfeaaae7
Yuzi0201 <2205816944@qq.com>
2022-06-03 00:46:18 +0800

    FIFO

--- EX3/Q1.c
+++ EX3/Q1.c
@@ -32,7 +32,7 @@ void init()
         list[i++] = m1 + 1;                               // 顺序执行一条指令，其地址为m’+1
         list[i] = rand() % (list_size - 2 - m1) + m1 + 2; // 在后地址[m’+2，319]中随机选取一条指令并执行(此处不能i++，因为for处有个i++)
     }
-    //将指令序列变换为页地址流
+    //将指令序列变换为页号
     for (size_t i = 0; i < list_size; i++)
     {
         list[i] /= 10;
@@ -56,14 +56,23 @@ void initPage(Page *page)
 }
 
 void OPT(Memory *memory, Page *pages, int *list, int current);
-void FIFO();
-void LRU();
+void FIFO(Memory *memory, Page *pages, int *list, int current);
+void LRU(Memory *memory, Page *pages, int *list, int current);
 
 int main()
 {
     init();
     Page pages[list_size / 10];
     Memory memory;
+    int method;
+    while (true)
+    {
+        printf("选择方法:\n1.OPT 2.FIFO 3.LRU\n");
+        scanf("%d", &method);
+        if (method == 1 || method == 2 || method == 3)
+            break;
+        printf("错误！重新输入:\n");
+    }
     for (size_t size = 4; size <= 32; size++) //用户内存容量为4页到32页
     {
         initMemory(&memory, size);
@@ -81,10 +90,20 @@ int main()
                 }
             if (!flag) //没有命中
             {
-                systemInterrupt++;
+                systemInterrupt++;              //缺页次数+1
                 if (memory.size == memory.used) //内存已满，使用置换
                 {
-                    OPT(&memory, pages, list, i);
+                    switch (method)
+                    {
+                    case 1:
+                        OPT(&memory, pages, list, i);
+                        break;
+                    case 2:
+                        FIFO(&memory, pages, list, i);
+                        break;
+                    case 3:
+                        LRU(&memory, pages, list, i);
+                    }
                 }
                 else // 储存页面
                 {
@@ -104,7 +123,7 @@ void OPT(Memory *memory, Page *pages, int *list, int current)
     int pageNum = list[current];
     int distance = 0;
     int j = 0;
-    int max;
+    int longestPageNum;
     for (int i = 0; i < memory->size; i++)
     {
         // 找到该页下次使用的序列号
@@ -116,16 +135,35 @@ void OPT(Memory *memory, Page *pages, int *list, int current)
         // 寻找下次使用时间最长的页
         if (j - current > distance)
         {
-            max = i;
+            longestPageNum = i;
             distance = j - current;
         }
     }
     // 替换页
-    memory->content[max] = pageNum;
+    memory->content[longestPageNum] = pageNum;
     pages[pageNum].saveTime = current;
     pages[pageNum].usedTime = current;
 }
 
-void FIFO() {}
+void FIFO(Memory *memory, Page *pages, int *list, int current)
+{
+    int pageNum = list[current];
+    int earliest = 114;
+    int earliestPageNum;
+    for (int i = 0; i < memory->size; i++) //寻找最早的页
+    {
+        if (pages[i].saveTime < earliest)
+        {
+            earliest = pages[i].saveTime;
+            earliestPageNum = i;
+        }
+    }
+    // 替换页
+    memory->content[earliestPageNum] = pageNum;
+    pages[pageNum].saveTime = current;
+    pages[pageNum].usedTime = current;
+}
 
-void LRU() {}
+void LRU(Memory *memory, Page *pages, int *list, int current)
+{
+}
